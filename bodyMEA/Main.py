a94d00f0159ee3562afd9f65112e4f44ae540afe
Lseoksee <da864268@naver.com>
2024-05-09 21:32:13 +0900

    눈 사이즈로 측정

--- bodyMEA/Main.py
+++ bodyMEA/Main.py
@@ -1,9 +1,11 @@
 # 모션 트래킹 코드
+from typing import Any
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+from torch import Tensor
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
 from ultralytics.engine.results import Results
@@ -15,8 +17,6 @@ BODY_PARTS = {"코": 0, "오른쪽 눈": 1, "왼쪽 눈": 2, "오른쪽 귀": 3,
 
 # 테스트 이미지
 img = cv.imread("bodyMEA/test.jpg")
-# 내 키
-myKey = 174
 
 # 점들간의 길이 구하는 함수
 def distance(points: list[tuple[float]]):
@@ -28,7 +28,7 @@ def distance(points: list[tuple[float]]):
     return distance
 
 # 기준 사물 높이 가지고 다른 사이즈 예측
-def __findRealSize(refSize: int, refPx, findPx):
+def findRealSize(refSize: int, refPx, findPx):
     cm_per_px = refSize / refPx
     return round(findPx * cm_per_px)
 
@@ -40,19 +40,53 @@ def personArea(img: cv.typing.MatLike, modelResult: Results):
     annotator.box_label(personBox, "", (0, 255, 0), (0, 0, 0))
     return annotator.result()
 
-# 픽셀상 사람 길이
-def personHight(modelResult: Results):
+# 사람 키 기준으로 측정하는 함수
+def heightToPoint(modelResult: Results, testDist: list[Tensor]):
+    # 내 키입력
+    MY_KEY = 174
+
     # 한 사람만 선택
     person = modelResult.boxes[0].xywh[0]
-    return int(person[3])
+
+    # 픽셀상 사람 길이
+    personPx = int(person[3])
+
+    # 구하려는 사이 픽셀 길이
+    dist = distance(testDist)
+
+    # 실제 길이 구하기
+    realSize = findRealSize(MY_KEY, personPx, dist)
+
+    print(f"픽셀길이: 구할려는거 {int(dist)}px, 픽셀상 사람 길이 {personPx}px")
+    print(f"실제 길이: {realSize}cm")
+    
+# 평균 눈 사이 거리로 측정하는 함수
+def eyeToPoint(modelResult: Results, testDist: list[Tensor]):
+    # 성인 평균 눈 사이 거리
+    EYE_TO_DIST = 7.5
+
+    # 한 사람만 선택
+    person = modelResult.boxes[0].xywh[0]
+
+    # 픽셀상 사람 길이
+    personPx = int(person[3])
+
+    # 눈 사이 픽셀상에 거리
+    eyePx = distance(testDist)
+
+    # 실제 길이 구하기
+    realSize = findRealSize(EYE_TO_DIST, eyePx, personPx)
+
+    print(f"눈사이 픽셀길이: {int(eyePx)}px, 픽셀상 사람 길이: {personPx}px")
+    print(f"실제 사람 길이 길이: {realSize}cm")
 
 # 포즈 구하는
 def pose(img: cv.typing.MatLike, modelSrc: str):
     model = YOLO(modelSrc)
     result: Results = model.predict(img)[0]
 
-    # result.masks 가 None 이면 이미지에 사람이 없는거
-    assert (not result.masks)
+    # 사람 감지 안되면 예외
+    assert (len(result.boxes.cls))
 
     # 여러 사람 감지 될 시 한사람만 되게
     person1Pose = result.keypoints.xy[0]
@@ -63,31 +97,28 @@ def pose(img: cv.typing.MatLike, modelSrc: str):
         person1Pose[BODY_PARTS["왼쪽 팔꿈치"]],
         person1Pose[BODY_PARTS["왼쪽 손목"]]
     ]
+    
+    # 눈 사이 거리
+    eyeTest = [
+        person1Pose[BODY_PARTS["오른쪽 눈"]],
+        person1Pose[BODY_PARTS["왼쪽 눈"]]
+    ]
 
     # 사람 영역 구하기
     img = personArea(img, result)
 
-    # 구하려는 사이 픽셀 길이
-    dist = distance(lenTest)
-
-    # 픽셀상 사람 길이
-    personPx = personHight(result)
-
-    # 실제 길이 구하기
-    realSize = __findRealSize(myKey, personPx, dist)
-
-    print(f"픽셀길이: 구할려는거 {int(dist)}px, 픽셀상 사람 길이 {personPx}px")
-    print(f"실제 길이: {realSize}cm")
-
-    point1 = lenTest[0]
-    point2 = lenTest[1]
-    point3 = lenTest[2]
+    # 사람 키 기준으로 측정하기
+    # heightToPoint(result, lenTest)
+    
+    # 평균 눈사이 거리로 측정하기
+    eyeToPoint(result, eyeTest)
+    
+    point1 = eyeTest[0]
+    point2 = eyeTest[1]
 
     # 시각화
     cv.ellipse(img, (int(point1[0]), int(point1[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
     cv.ellipse(img, (int(point2[0]), int(point2[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
-    cv.ellipse(img, (int(point3[0]), int(point3[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
-
     return img
 
 
