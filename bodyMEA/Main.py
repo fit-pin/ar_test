678112c39c2daa4be20368b0815bb1e0d567b9fe
Lseoksee <da864268@naver.com>
2024-05-10 13:50:49 +0900

    점 연결 배열 완성

--- bodyMEA/Main.py
+++ bodyMEA/Main.py
@@ -15,6 +15,11 @@ BODY_PARTS = {"코": 0, "오른쪽 눈": 1, "왼쪽 눈": 2, "오른쪽 귀": 3,
               "왼쪽 손목": 10, "오른쪽 골반": 11, "왼쪽 골반": 12, "오른쪽 무릎": 13, "왼쪽 무릎": 14,
               "오른쪽 발": 15, "왼쪽 발": 16}
 
+# 상체 점 연결
+PARES_TOP = {"팔 길이": ["왼쪽 어깨", "왼쪽 팔꿈치", "왼쪽 손목"], "어께너비": ["왼쪽 어깨", "오른쪽 어깨"], "상체너비": ["왼쪽 어깨", "왼쪽 골반"]}
+# 하체 점 연결
+PARES_BOTTOM = {"다리 길이": ["왼쪽 골반", "왼쪽 무릎", "왼쪽 발"]}
+
 # 테스트 이미지
 img = cv.imread("bodyMEA/test.jpg")
 
@@ -59,7 +64,7 @@ def heightToPoint(modelResult: Results, testDist: list[Tensor]):
 
     print(f"픽셀길이: 구할려는거 {int(dist)}px, 픽셀상 사람 길이 {personPx}px")
     print(f"실제 길이: {realSize}cm")
-    
+
 # 평균 눈 사이 거리로 측정하는 함수
 def eyeToPoint(modelResult: Results, testDist: list[Tensor]):
     # 성인 평균 눈 사이 거리
@@ -97,7 +102,7 @@ def pose(img: cv.typing.MatLike, modelSrc: str):
         person1Pose[BODY_PARTS["왼쪽 팔꿈치"]],
         person1Pose[BODY_PARTS["왼쪽 손목"]]
     ]
-    
+
     # 눈 사이 거리
     eyeTest = [
         person1Pose[BODY_PARTS["오른쪽 눈"]],
@@ -109,16 +114,20 @@ def pose(img: cv.typing.MatLike, modelSrc: str):
 
     # 사람 키 기준으로 측정하기
     # heightToPoint(result, lenTest)
-    
+
     # 평균 눈사이 거리로 측정하기
     eyeToPoint(result, eyeTest)
-    
+
     point1 = eyeTest[0]
     point2 = eyeTest[1]
 
     # 시각화
-    cv.ellipse(img, (int(point1[0]), int(point1[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
-    cv.ellipse(img, (int(point2[0]), int(point2[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
+    for key in BODY_PARTS.keys():
+        points = person1Pose[BODY_PARTS[key]]
+        cv.putText(img, str(BODY_PARTS[key]), (int(points[0]), int(points[1])),
+                   cv.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 2, cv.LINE_4)
+        cv.ellipse(img, (int(points[0]), int(points[1])), (8, 8), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
     return img
 
 
