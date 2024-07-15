a02316fc428b5ae048f6ca7aec77bbbb7022180a
Lseoksee <da864268@naver.com>
2024-07-15 16:06:54 +0900

    IDM VTON API 테스트

---
+++ modelTest/bodyMEA/IDM-VTON-API.py
@@ -0,0 +1,26 @@
+from gradio_client import Client, handle_file
+from os import remove, path
+from shutil import rmtree
+from matplotlib import pyplot
+
+HUMAN_IMG = "bodyMEA/res/test2.jpg"
+CLOTHE_IMG = "bodyMEA/res/clothes_top.jpg"
+
+client = Client("kadirnar/IDM-VTON", download_files="./bodyMEA/res/gradio/")
+result = client.predict(
+		dict={"background": handle_file(HUMAN_IMG),"layers":[],"composite":  None},
+		garm_img=handle_file(CLOTHE_IMG),
+		garment_des="Hello!!",
+		is_checked=True,
+		is_checked_crop=True,
+		denoise_steps=30,
+		seed=42,
+		api_name="/tryon"
+)
+
+# 마스킹 이미지 제거
+rmtree(path.dirname(result[1]))
+
+img = pyplot.imread(result[0])
+pyplot.imshow(img)
+pyplot.show()
