aafce31a7638f2c4d87c77e3cc40576429a41683
Lseoksee <da864268@naver.com>
2024-09-28 17:22:10 +0900

    if main 추가

--- clothesMEA/src/cardDict_train.py
+++ clothesMEA/src/cardDict_train.py
@@ -1,7 +1,11 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("model/yolov8m.pt") 
 
-train = model.train(data="dataset/data.yaml", epochs=100)
+def main():
+    # Load a model
+    model = YOLO("model/yolov8m.pt") 
 
+    train = model.train(data="dataset/data.yaml", epochs=100)
+
+if __name__ == "__main__":
+    main()
